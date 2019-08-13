2bb10c253b3c63279e56a2e7591293ec4d3edb93
Devender <myaka.devenderreddy@gmail.com>
2019-08-13 11:18:28 +0530

    changes

--- database.config.js
+++ database.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- 	 url: 'mongodb://localhost:27017/avenirinfo'
-	//url:'mongodb+srv://sumanth:avenir@123@cluster0-ztxs6.mongodb.net/test?retryWrites=true&w=majority'
+ 	 //url: 'mongodb://localhost:27017/avenirinfo'
+	url:'mongodb+srv://sumanth:avenir@123@cluster0-ztxs6.mongodb.net/test?retryWrites=true&w=majority'
 	 //url: 'mongodb+srv://avenir:avenir1234@cluster0-ksxk6.mongodb.net/test?retryWrites=true&w=majority'
 	//"db" : "Cluster0"//db
 }
